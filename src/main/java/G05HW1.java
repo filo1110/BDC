8c4671e5a2c86c5cae5e98d092d0014e3baca86a
filo1110 <81578414+filo1110@users.noreply.github.com>
2021-03-29 18:09:32 +0200

    Homework1

---
+++ src/main/java/G05HW1.java
@@ -0,0 +1,34 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class G05HW1 {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            throw new IllegalArgumentException("USAGE: num_partitions file_path");
+        }
+
+        SparkConf conf = new SparkConf(true).setAppName("WordCount");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("WARN");
+
+        int K = Integer.parseInt(args[0]);
+
+        JavaRDD<String> docs = sc.textFile(args[1]).repartition(K).cache();
+
+        long numdocs, numwords;
+        numdocs = docs.count();
+        System.out.println("Number of documents = " + numdocs);
+        JavaPairRDD<String, Long> count;
+        Random randomGenerator = new Random();
+    }
+}
